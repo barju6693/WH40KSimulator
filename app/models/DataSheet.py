037c944b15e6338a0b4e7bb1c9b6fcad64108c87
Sergi <F1barju@gmail.com>
2024-11-12 16:29:25 +0100

    seguimos con la definicion de las fichas de datos

--- app/models/DataSheet.py
+++ app/models/DataSheet.py
@@ -1,9 +1,19 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from enum import Enum
 
+class Hability(BaseModel):
+    name: str = Field(description="")
+    description: str = Field(description="")
 
-class Habilities(BaseModel):
-    pass
+class WargearAbilities(BaseModel):
+    name: str
+    description: str
+
+class Abilities(BaseModel):
+    core: str = Field(description="")
+    faction: str = Field(description="")
+    habilities_list: list[Hability] = Field(description="")
+    invulnerable_save: str = Field(description="")
 
 
 class WeaponsType(str, Enum):
@@ -12,28 +22,60 @@ class WeaponsType(str, Enum):
 
 
 class Weapons(BaseModel):
-    range = int
-    attacks = int
-    weapon_skill = int
-    ballistic_skill = int
-    strength = int
-    armour_penetration = int
-    damage = int
+    weapons_type: WeaponsType
+    range: int
+    attacks: int
+    weapon_skill: int
+    ballistic_skill: int
+    strength: int
+    armour_penetration: int
+    damage: int
+
+    @field_validator("range", mode="before")
+    def check_range(cls,v, values):
+        if values["weapons_type"] == WeaponsType.ranged:
+            return v
+        else:
+            return 0
+
 
 
 class Keywords(str, Enum):
-    pass
+    infantry = "Infantry"
+    character = "Character"
+    epic_hero = "Epic hero"
+    terminator = "Terminator"
+    chapter_master = "Chapter master"
+    logan_grimnar = "Logan Grimnar"
+    battleline = "Battleline"
+    grenades = "Grenades"
+    imperium = "Imperium"
+    grey_hunters = "Grey hunters"
+    blood_claws = "Blood claws"
+
+
 
 class FactionKeywords(str, Enum):
-    pass
+    adeptus_astartes = "Adeptus Astartes"
+    space_wolves = "Space Wolves"
+    black_templars = "Black Templars"
+    dark_angels = "Dark Angels"
+    chaos_demons = "Chaos Daemons"
+    space_marines = "Space Marines"
+    grey_knights = "Grey Knights"
 
 
 class WargearOptions(BaseModel):
-    pass
+    what_repleced: str = Field(description="")
+    oprtion: dict[str, int]
 
 
 class UnitComposition(BaseModel):
-    pass
+    amount_min: int
+    amount_max: int
+    name: str
+    description: str
+    attached_unit: str
 
 
 class Profiles(BaseModel):
@@ -49,7 +91,7 @@ class Profiles(BaseModel):
 class DataSheet(BaseModel):
     name: str = Field(description="")
     profile: Profiles = Field(description="")
-    habilities: Habilities = Field(description="")
+    Abilities: Abilities = Field(description="")
     weapons: list[Weapons] = Field(description="")
     faction_keywords: list[FactionKeywords] = Field(description="")
     keywords: list[Keywords] = Field(description="")
